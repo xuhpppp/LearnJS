aed01582fd7c495a62d337f4fb29351a727c5353
xuhp <doanthephuc.ST@gmail.com>
2022-09-23 10:51:11 +0700

    static file: css, js, image

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config;
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 configViewEngine(app);
 
